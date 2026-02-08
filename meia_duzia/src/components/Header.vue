f781d2c70d85bb13d058c0032675d18032892760
maju <julia.gomes1@escolar.ifrn.edu.br>
2026-02-07 21:50:50 -0300

    Instalação do vue

---
+++ meia_duzia/src/components/Header.vue
@@ -0,0 +1,30 @@
+<script setup>
+defineProps({
+    selected_page: {
+        type: String,
+        required: true,
+        default: "Home"
+    }
+})
+
+const pages = ["Home", "Page 1", "Page 2"]
+</script>
+
+
+<template>
+<h1>MeiaDuzia</h1>
+<nav>
+    <ul>
+        <li v-for="page in pages" :key="page">
+            :key = "page"
+            
+        </li>
+    </ul>
+</nav>
+
+</template>
+
+
+<style scoped>
+
+</style>
