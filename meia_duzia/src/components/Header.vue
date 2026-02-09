016edb3446a0606a033b34e606526eb82f04e9dc
Giordanni <giordannimaciel@gmail.com>
2026-02-09 16:21:25 -0300

    hedaer

--- meia_duzia/src/components/Header.vue
+++ meia_duzia/src/components/Header.vue
@@ -4,6 +4,7 @@
 import logo from '@/assets/logo.png'
 import carrinho_de_compras from '@/assets/icons/carrinho.png'
 import favoritos from '@/assets/icons/favoritos.png'
+import lupa from '@/assets/icons/lupa.png'
 
 </script>
 
@@ -11,17 +12,31 @@ import favoritos from '@/assets/icons/favoritos.png'
 <!------------------------------ HTML ------------------------------>
 <template>
 <header>
+<div class="container">
+
 
 <div id="esquerda">
+
     <h1> <img :src="logo" alt="Logo"> </h1>
+
+    <form action="">
+        <div id="procurar_objeto">
+            <input type="text" placeholder="Procurar...">
+            
+            <a href=""> 
+                <img :src="lupa" alt="Lupa">
+            </a>
+        </div>
+    </form>
+
 </div>
 
 
 <div id="direita">
+    <button id="entrar"> Entrar </button>
+
     <nav>
     <ul>
-        <li id="entrar"> <a href="#"> Entrar </a> </li>
-        
         <li class="icon">
             <a href="#">
                 <img :src="carrinho_de_compras" alt="Carrinho de Compras"/>
@@ -37,6 +52,8 @@ import favoritos from '@/assets/icons/favoritos.png'
     </nav>
 </div>
 
+
+</div>
 </header>
 </template>
 
@@ -44,32 +61,14 @@ import favoritos from '@/assets/icons/favoritos.png'
 <!------------------------------ CSS ------------------------------>
 <style scoped>
 
-a {
-    text-decoration: none;
-    color: white;
-}
-
-header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-
-    padding: 20px;
+header{
     background-color: #d38787;
 }
 
-nav ul {
-    list-style: none;
-    display: flex;
-    gap: 20px;
-}
 
 #entrar {
-    display: flex;
-    align-items: center;
-
     background-color: #992525;
-    border-radius: 5px;
+    color: white;
 }
 
 </style>
